826ebfbf2d0ec2a702afa3328cd9689a0e0b2a7d
mid-eum <mid-eum@lisia-TK>
2015-09-04 19:56:14 +0900

    주석 달기 마침

--- src/Problem03.java
+++ src/Problem03.java
@@ -1,10 +1,9 @@
 import lfsr.LFSR;
 
 public class Problem03 {
-	@SuppressWarnings("null")
 	public static void main(String[] args)
 	{
-		Problem03 pr03 = new Problem03();
+		Problem03 test = new Problem03();
 		String[] a = {
 				"01000101", "01010101", "01100000", "01110000",
 				"10000100", "10010100", "10100001", "10110001",
@@ -17,24 +16,23 @@ public class Problem03 {
 		for(int i=0;i<a.length;i++)
 		{
 			LFSR lfsr = new LFSR(a[i],seed);
-			//lfsr.testPrint();
 			keyPredict[i] = lfsr.keyGen(48);
 			System.out.println((i+1)+"th Key: "+keyPredict[i]);
 		}
 		
 		System.out.println("========== verify ==========");
-		pr03.verify(keyPredict);
+		test.verify(keyPredict);
 	}
-	private void verify(String[] test)
+	private void verify(String[] keys)
 	{
-		for(int i=0;i<test.length;i++)
+		for(int i=0;i<keys.length;i++)
 		{
-			if(isPromissing(test[i]))
+			if(isPromissing(keys[i]))
 				System.out.println((i+1)+" is PROMISSING!");
 		}
 	}
-	private boolean isPromissing(String keys)
+	private boolean isPromissing(String key)
 	{
-		return keys.substring(30, 34).equals("0000");
+		return key.substring(30, 34).equals("0000");
 	}
 }
--- src/lfsr/LFSR.java
+++ src/lfsr/LFSR.java
@@ -2,6 +2,11 @@ package lfsr;
 
 import java.util.BitSet;
 
+/**
+ * Linear Feedback Shift Register
+ * @author trustkim
+ * 기본 자료구조를 BitSet을 사용하여 구현함. Java의 BitSet은 사용하기 헷갈리고 불편하다.
+ */
 public class LFSR {
 	private BitSet a;	// feedback coefficient
 	private BitSet reg;	// register
@@ -9,9 +14,9 @@ public class LFSR {
 	
 	/* Generating methods */
 	public LFSR(String aStr, String seedStr)
-	{	// "10000011", "10100101"을 long 정수로 바꿔 valueOf로 BitSet으로 바꿔 저장
-		regLen = seedStr.length();
-		a = fromString(aStr);
+	{
+		regLen = seedStr.length();	// 레지스터 길이는 seed 길이에 맞게 최초 세팅해 줌
+		a = fromString(aStr);		// initialize the feedback coefficient from aStr
 		reg = fromString(seedStr);	// initialize the registers from seedStr
 	}
 	private BitSet fromString(final String s)
@@ -22,23 +27,17 @@ public class LFSR {
 	
 	/* functioning methods */
 	private boolean step()
-	{ // LFSR 한 번 쉬프트 하는 과정
-		boolean result; // 새로 생성한 키
+	{ // LFSR 한 번 쉬프트 하는 과정. linear function.
+		boolean result; // 현재 스텝의 결과
 		result = (a.get(0) && reg.get(0));
-		//System.out.println("[debug] before step: "+result);
-		//System.out.println(toString(reg));
 		for(int i=1;i<regLen;i++)	// 레지스터 길이 만큼 반복
 		{
-			//System.out.println("last result: "+result+", now: "+(a.get(i)&&reg.get(i)));
-			//System.out.println("a["+i+"] AND reg["+i+"] = "+a.get(i)+" AND "+reg.get(i)+" = "+(a.get(i)&&reg.get(i)));
 			result ^= (a.get(i)&&reg.get(i));
-			//System.out.println("[debug] step: ["+i+"]: "+result);
 		}
-		//System.out.println(result);
 		return result;
 	}
 	public String keyGen(int n)
-	{	// LFSR을 이용하여 길이 n개의 키를 생성
+	{	// LFSR을 이용하여 길이 n개의 키를 생성. seed가 최상위부터 채워짐.
 		BitSet keyStream = reg.get(0, regLen);	// step 하기 전에 reg에 저장된 seed를 먼저 복사해 둠
 		System.out.println("feedback coefficiant: "+toString(a)+" ("+(a.size()/8)+")");
 		for(int i=0;i<n-regLen;i++)
@@ -48,42 +47,34 @@ public class LFSR {
 			{
 				keyStream.set(0);
 			}
-			//System.out.println(toString(keyStream));
 			updateReg(keyStream);
 		}
 		return toString(keyStream);
 	}
 	private BitSet leftShift(BitSet bs)
-	{
+	{	// 키스트림을 모두 왼쪽쉬프트함. 가중치가 가장 낮은 최하위 비트에는 step()의 결과 비트를 채워 넣음.
 		BitSet result = new BitSet(bs.length()+1);
 		for(int i=0;i<bs.length();i++)
 		{
 			if(bs.get(i))
 				result.set(i+1);
 		}
-		//System.out.println("after leftShift: "+toString(result));
 		return result;
 	}
 	private void updateReg(BitSet ks)
-	{
+	{	// update register
 		reg = new BitSet(regLen);
 		for(int i=0;i<regLen;i++)
 		{
 			if(ks.get(i))
 				reg.set(i);
 		}
-		//System.out.println("updated: "+toString(reg));
 	}
 
-	
+
 	/* Printing methods */
 	private String toString(BitSet bs)
-	{	// BitSet객체를 Long 타입으로 바꿔 2진수 형식으로 반환
+	{	// BitSet객체를 Long 타입으로 바꿔 2진수 형식으로 반환. 상위비트가 0이오면 출력안됨 주의해야함.
 		return Long.toString(bs.toLongArray()[0], 2);
-	}
-	public void testPrint() {
-		// 테스트를 위한 출력
-		System.out.println("feedback coefficiant: "+toString(a));
-		System.out.println("seed: "+toString(reg));
 	}
 }
